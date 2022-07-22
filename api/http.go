8d7e5d0c97a6d163581cb2f717b8b2ffe65ceca0
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2022-07-22 10:34:13 +0700

    [Wat][Edited] - try to place file on root path instead of inside the dir

--- api/http.go
+++ api/http.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func HTTPPackageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	fmt.Fprintf(w, `{"msg":"Hello World!"}`)
 }
